robertsilen/oar

commit 10a9216761c236f6e4bf6c07b8bf1c58863d2251

[IO][meta_sched] Added number of jobs runnin,waiting, and no-reservation+waiting to logs

sources/core/common-libs/lib/OAR/IO.pm
@@ -10126,6 +10126,7 @@ sub format_stats(\@\@) {
    my @start = shift;
    my @end = shift;
    my ($time, $usec, $ssec, $cusec, $cssec, $trans) = array_substract(@end, @start);
+   my ($time, $usec, $ssec, $cusec, $cssec) = map sprintf("%.2f",$_), ($time, $usec, $ssec, $cusec, $cssec);
    return "elapsed:${time}s user:${usec}s sys:${ssec}s child_user:${cusec}s child_sys:${cssec}s ; approx ${trans} DB transactions";
 }
 
@@ -10143,6 +10144,70 @@ sub array_minus(\@@) {
     my @b = @_;
     my %e = map { $_ => undef } @b;
     return grep(!exists($e{$_}), @$a);
+}
+
+
+sub get_count_jobs_active_queues($){
+    my $dbh         = shift;
+    my @queues      = get_active_queues($dbh);
+    my $queue_names = join ',', map "\'$_->[0]\'", @queues;
+    my @states      = ( "Running", "Waiting" );
+    my @reservations = ( "None", "Scheduled" );
+
+
+    my $req = <<EOS;
+SELECT queue_name,state,reservation,COUNT(job_id)
+FROM jobs
+WHERE state IN (\'Running\',\'Waiting\')
+AND queue_name IN ( $queue_names )
+GROUP BY state,reservation,queue_name
+ORDER BY queue_name,state
+EOS
+
+    my $sth = $dbh->prepare($req);
+    $sth->execute();
+    my %res = ();
+    foreach my $queue (@queues){
+        foreach ( "Running", "Waiting", "Waiting(NoSched)" ){
+            $res{$queue->[0]}{$_} = 0;
+        }
+    }
+    while (my $ref = $sth->fetchrow_hashref()) {
+        $res{$ref->{'queue_name'}}{$ref->{'state'}}+=$ref->{'count'};
+        if (($ref->{'state'} eq 'Waiting') and ($ref->{'reservation'} eq 'None')) {
+            $res{$ref->{'queue_name'}}{'Waiting(NoSched)'}+=$ref->{'count'};
+        }
+    }
+    $sth->finish();
+    return (%res);
+
+}
+
+sub format_count_jobs_active_queue(%) {
+    my %job_counts = @_;
+    my @res = ();
+
+    foreach my $queue (sort keys %job_counts){
+        my $counts = "Jobs counts in: ". $queue ."=>";
+        foreach my $group (sort keys %{$job_counts{$queue}}) {
+            $counts .= $group . ":" . $job_counts{$queue}{$group} . ",";
+        }
+        chop($counts);
+        push @res, $counts;
+    }
+    return @res;
+}
+
+sub format_count_jobs_of_queue($%) {
+    my $queue      = shift();
+    my %job_counts = @_;
+    my $res        = "";
+
+    foreach my $group (sort keys %{$job_counts{$queue}}) {
+        $res .= $group . ":" . $job_counts{$queue}{$group} . ",";
+    }
+    chop($res);
+    return $res;
 }
 
 # Return an array without the duplicates

sources/core/modules/scheduler/oar_meta_sched
@@ -106,6 +106,12 @@ my $Walltime_change_enabled = uc(get_conf_with_default_param("WALLTIME_CHANGE_EN
 oar_info($Module_name, "Starting Meta Scheduler\n", $Session_id);
 my @stats_start = OAR::IO::get_stats($base);
 
+my %start_job_counts = OAR::IO::get_count_jobs_active_queues($base_ro);
+my @job_count_logs = OAR::IO::format_count_jobs_active_queue(%start_job_counts);
+foreach (@job_count_logs){
+oar_info($Module_name,$_ . "\n",$Session_id);
+}
+
 ###########################################################################################################
 # Handle walltime change requests                                                                              #
 ###########################################################################################################
@@ -1526,7 +1532,8 @@ foreach my $queue (@queues) {
 
         my @stats_queue_end = OAR::IO::get_stats($base);
         my $stats_queue_str = OAR::IO::format_stats(@stats_queue_start, @stats_queue_end);
-        oar_info($Module_name, "End of processing for Queue $name ($stats_queue_str)\n", $Session_id);
+        my $start_queue_count_str = OAR::IO::format_count_jobs_of_queue($name,%start_job_counts);
+        oar_info($Module_name, "End of processing for Queue $name ($stats_queue_str ; $start_queue_count_str)\n", $Session_id);
 
     } else {
         oar_info($Module_name, "Queue $name: No job\n", $Session_id);

commit 54ef72021873d7f0732ad3190d6cae58252b1099

[IO] Fixed bug introduced in 10a9216761c236f6e4bf6c07b8bf1c58863d2251

sources/core/common-libs/lib/OAR/IO.pm
@@ -10126,7 +10126,7 @@ sub format_stats(\@\@) {
    my @start = shift;
    my @end = shift;
    my ($time, $usec, $ssec, $cusec, $cssec, $trans) = array_substract(@end, @start);
-   my ($time, $usec, $ssec, $cusec, $cssec) = map sprintf("%.2f",$_), ($time, $usec, $ssec, $cusec, $cssec);
+   ($time, $usec, $ssec, $cusec, $cssec) = map sprintf("%.2f",$_), ($time, $usec, $ssec, $cusec, $cssec);
    return "elapsed:${time}s user:${usec}s sys:${ssec}s child_user:${cusec}s child_sys:${cssec}s ; approx ${trans} DB transactions";
 }
 